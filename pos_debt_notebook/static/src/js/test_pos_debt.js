5cdb3a62eafab8e6c3a08e21b22e425b869f89b6
fedoranvar <jakildebek@gmail.com>
2019-09-17 13:12:38 +0500

    :shield: unit-tour-test

--- pos_debt_notebook/static/src/js/test_pos_debt.js
+++ pos_debt_notebook/static/src/js/test_pos_debt.js
@@ -80,5 +80,6 @@ odoo.define('pos_debt_notebook.tour', function (require) {
     steps = steps.concat(add_product_to_order('Miscellaneous'));
     steps = steps.concat(set_customer('Agrolait'));
     steps = steps.concat(debt_method_paying('Credits (USD)'));
+    https://github.com/it-projects-llc/pos-addons/issues/592
     tour.register('tour_pos_debt_notebook', { test: true, url: '/web' }, steps);
 });
